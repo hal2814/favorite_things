d09d4bd70fd3f4f96308b91e15efc951340d436e
Riki Montgomery <mostriki820@gmail.com>
2017-09-13 14:27:08 -0700

    create files

---
+++ spec/item_spec.rb
@@ -0,0 +1,34 @@
+require('pry')
+require('rspec')
+require('item')
+
+# describe Triangles do
+describe("Item") do
+  before() do
+    Item.clear()
+  end
+
+  describe(".all") do
+    it("is empty at first") do
+      expect(Item.all()).to(eq([]))
+    end
+  end
+
+
+  describe("#save") do
+    it("saves an item to the list of items") do
+      item = Item.new("tacos")
+      item.save()
+      expect(Item.all()).to(eq([item]))
+    end
+  end
+
+  describe(".clear") do
+    it("clears all items from the list") do
+      item = Item.new("tacos")
+      item.save()
+      Item.clear()
+      expect(Item.all()).to(eq([]))
+    end
+  end
+end
